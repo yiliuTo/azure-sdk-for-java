4210f155332a0a20f66658c5a9123110c124c8a9
Yi Liu <yiliu6@microsoft.com>
2021-01-29 13:15:23 +0800

    add on-demand test

--- sdk/spring/azure-spring-boot-test-aad/src/test/java/com/azure/test/aad/selenium/ondemand/AADOnDemandIT.java
+++ sdk/spring/azure-spring-boot-test-aad/src/test/java/com/azure/test/aad/selenium/ondemand/AADOnDemandIT.java
@@ -3,55 +3,55 @@
 
 package com.azure.test.aad.selenium.ondemand;
 
+import com.azure.test.aad.selenium.AADSeleniumITHelper;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 import static com.azure.spring.test.EnvironmentVariable.AAD_USER_NAME_ON_DEMAND;
-import static com.azure.spring.test.EnvironmentVariable.AAD_USER_NAME_ON_DEMAND_FAKE;
-import static com.azure.spring.test.EnvironmentVariable.AAD_USER_PASSWORD_1;
+import static com.azure.spring.test.EnvironmentVariable.AAD_USER_PASSWORD_ON_DEMAND;
+import static com.azure.test.aad.selenium.AADSeleniumITHelper.createDefaultProperties;
 
 public class AADOnDemandIT {
-    private static final Logger LOGGER = LoggerFactory.getLogger(com.azure.test.aad.selenium.ondemand.AADOnDemandIT.class);
-//    private AADSeleniumITHelper aadSeleniumITHelper;
+    private static final Logger LOGGER =
+        LoggerFactory.getLogger(com.azure.test.aad.selenium.ondemand.AADOnDemandIT.class);
+    private AADSeleniumITHelper aadSeleniumITHelper;
 
     @Test
     public void onDemandTest() {
-        Map<String, String> properties = new HashMap<>();
-        properties.put("azure.activedirectory.client-id", AAD_USER_NAME_ON_DEMAND);
-        properties.put("azure.activedirectory.client-secret", AAD_USER_PASSWORD_1);
-        String onDemandUser1 = AAD_USER_NAME_ON_DEMAND;
-        LOGGER.info(onDemandUser1);
-        String onDemandUser2 = AAD_USER_NAME_ON_DEMAND_FAKE;
-        LOGGER.info(onDemandUser2);
+        Map<String, String> properties = createDefaultProperties();
+        properties.put("azure.activedirectory.authorization-clients.arm.scopes",
+            "https://management.azure.com/user_impersonation");
+        properties.put("azure.activedirectory.authorization-clients.arm.on-demand", "true");
 
-        String username1 = onDemandUser1.split("@")[0];
-        LOGGER.info(username1);
-        String username2 = onDemandUser2.split("@")[0];
-        LOGGER.info(username2);
+        aadSeleniumITHelper = new AADSeleniumITHelper(com.azure.test.aad.selenium.ondemand.AADOnDemandIT.class,
+            properties,
+            AAD_USER_NAME_ON_DEMAND, AAD_USER_PASSWORD_ON_DEMAND);
+        aadSeleniumITHelper.logIn();
 
-        //        aadSeleniumITHelper = new AADSeleniumITHelper(com.azure.test.aad.selenium.ondemand.AADOnDemandIT.class, properties,
-//            AAD_USER_NAME_2, AAD_USER_PASSWORD_2);
-//        aadSeleniumITHelper.logIn();
-//
-//        String httpResponse = aadSeleniumITHelper.httpGet("api/home");
-//        Assert.assertTrue(httpResponse.contains("home"));
+        String httpResponse = aadSeleniumITHelper.httpGet("api/home");
+        Assert.assertTrue(httpResponse.contains("home"));
     }
 
     @After
     public void destroy() {
-//        aadSeleniumITHelper.destroy();
+        aadSeleniumITHelper.destroy();
     }
 
     @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
@@ -59,10 +59,16 @@ public class AADOnDemandIT {
     @RestController
     public static class DumbApp {
 
-        @GetMapping(value = "/api/home")
-        public ResponseEntity<String> home(Principal principal) {
-            LOGGER.info(((OAuth2AuthenticationToken) principal).getAuthorities().toString());
+        @GetMapping(value = "accessTokenScopes/azure")
+        public ResponseEntity<String> azure(
+            @RegisteredOAuth2AuthorizedClient("azure") OAuth2AuthorizedClient authorizedClient) {
             return ResponseEntity.ok("home");
+        }
+
+        @GetMapping(value = "accessTokenScopes/arm")
+        public ResponseEntity<String> arm(
+            @RegisteredOAuth2AuthorizedClient("arm") OAuth2AuthorizedClient authorizedClient) {
+            return ResponseEntity.ok("arm");
         }
     }
 }
